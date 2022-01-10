d7a56a4a6d7c15134476457192cd9a7132124fcd
thunk <andy.brownlie@digital54.co>
2022-01-10 15:07:05 +0000

    removed databaseURL from config as only required for Realtime db and not Firestore

--- modules/firebase-web-app/module.tf
+++ modules/firebase-web-app/module.tf
@@ -16,7 +16,6 @@ data "template_file" "firebase_config" {
     app_id              = google_firebase_web_app.basic.app_id
     api_key             = data.google_firebase_web_app_config.basic.api_key
     auth_domain         = data.google_firebase_web_app_config.basic.auth_domain
-    database_url        = lookup(data.google_firebase_web_app_config.basic, "database_url", "")
     project_id          = var.firebase_project_id
     storage_bucket      = lookup(data.google_firebase_web_app_config.basic, "storage_bucket", "")
     messaging_sender_id = lookup(data.google_firebase_web_app_config.basic, "messaging_sender_id", "")
